b4ba3475d159426dcc19678b45c81a8e46b7854f
Elod Tobak <tobakelod@gmail.com>
2023-06-06 18:44:06 +0200

    remove run function

--- package/src/index.ts
+++ package/src/index.ts
@@ -3,20 +3,16 @@ import handle from "./middleware.js";
 import "dotenv/config";
 import setup from "./setup.js";
 
-async function run() {
-  await setup();
+await setup();
 
-  const PORT = 3000;
+const PORT = 3000;
 
-  const app = express();
+const app = express();
 
-  app.use(handle);
+app.use(handle);
 
-  app.use(express.static(process.env.SERVER_PATH ?? "./server"));
+app.use(express.static(process.env.SERVER_PATH ?? "./server"));
 
-  app.listen(PORT, () => {
-    console.log(`Astro ISR Server listening on port ${PORT}`);
-  });
-}
-
-run();
+app.listen(PORT, () => {
+  console.log(`Astro ISR Server listening on port ${PORT}`);
+});
